ad8ab3b505a64bb821a236b31cdaab0fa726d815
Mustafa Mat <mmat@technoplatz.de>
2023-03-06 12:33:36 +0100

    Updated script and compose

--- bi.sh
+++ bi.sh
@@ -33,209 +33,144 @@
 
 cat << EOF
 
-Technoplatz BI - Open Source Data Sharing Platform
+Technoplatz BI
+Data Management and Sharing Platform
 Copyright 2019-$(date +'%Y'), Technoplatz IT Solutions GmbH
 https://bi.technoplatz.com
 
 EOF
 
-function listAllCommands() {
-    echo "Available commands:"
-    echo "./bi.sh install [token]"
-    echo "./bi.sh up [--build] [--prod]"
+function helpBI() {
+    echo "Available commands;"
+    echo
+    echo "./bi.sh install"
+    echo "./bi.sh up"
     echo "./bi.sh down"
-    echo "./bi.sh kill"
-    echo "./bi.sh pull [--prod]"
-    echo "./bi.sh build <image | --all> [--prod]"
+    echo "./bi.sh update"
     echo "./bi.sh prune"
     echo "./bi.sh help"
-    echo
-    echo "See more at https://bi.technoplatz.com/support#script-commands-reference"
-}
-
-function getSuffixBI() {
-    SUFFIX=""
-    BRANCH=$(git branch --show-current)
-    if [[ ! -z $BRANCH ]]; then
-        if [ $BRANCH != "main" ]; then
-            SUFFIX="-$BRANCH"
-        fi
-        if [ $2 ]; then
-            if [ $1 == "up" ]; then
-                if [ $2 != "--build" ]; then 
-                    SUFFIX="-1"
-                else
-                    if [ $3 ]; then
-                        if [ $3 != "--prod" ]; then SUFFIX="-1"; else SUFFIX=""; fi
-                    fi
-                fi
-            elif [ $1 == "build" ]; then
-                if [ $3 ]; then
-                    if [ $3 != "--prod" ]; then SUFFIX="-1"; else SUFFIX=""; fi
-                fi
-            else
-                if [ $2 != "--prod" ]; then SUFFIX="-1"; else SUFFIX=""; fi
-            fi
-        fi
-    fi
-    echo $SUFFIX
-    return 1
-}
-
-function getFiles() {
-    declare -a curlHeaders=("-H" "Accept: application/vnd.github.v3.raw")
-    if [ $1 ]; then
-        curlHeaders+=("-H" "Authorization: token $1")
-    else
-        echo "Process will continue without GitHub token."
-    fi
-    curl "${curlHeaders[@]}" -Ls -o $DCYML -o $DOTENV https://raw.githubusercontent.com/Technoplatz/bi/main/{$DCYML,$DOTENV}
-    return 1
 }
 
 function installBI() {
     if [ -d $DIR ]; then
-        echo "Oops! \"$DIR\" directory already exists. You should remove it manually."
-        echo "Installation was canceled to avoid any data loss."
-        return 0
-    else
-        mkdir $DIR
-        mkdir $DIR/_init
+        while true; do
+            read -p "$(echo -e "Oops! An existing installation found in ${COLOR}$DIR${NOCOLOR} folder. Do you want to remove it? (Y/n) ")" yn
+            case $yn in
+                [Y]* )
+                    rm -rf $DIR
+                    break;;
+                [Nn]* )
+                    echo
+                    echo "Installation canceled."
+                    return 0
+                    ;;
+                * ) 
+                    echo; echo "Please enter Y or n!"; echo
+                    ;;
+            esac
+        done
     fi
+    mkdir -p $DIR
+    mkdir -p $DIR/_init && touch $DIR/_init/.secret-key-password
+    echo
+    echo "✔ System folder created successfuly."
     cd $DIR
-    getFiles "$1"
+    declare -a curlHeaders=("-H" "Accept: application/vnd.github.v3.raw")
+    if [ $1 ]; then
+        curlHeaders+=("-H" "Authorization: token $1")
+    fi
+    curl "${curlHeaders[@]}" -Ls -o $DCYML -o $DOTENV https://raw.githubusercontent.com/Technoplatz/bi/main/{$DCYML,$DOTENV}
     CONTD=$(cat $DCYML | head -c 3)
     if [[ "$CONTD" == *"40"* ]]; then
-        echo "Required files not found on GitHub ($CONTD)."
-        echo "You may provide a token to get connected to the repository."
+        echo "Required files were not received successfully ($CONTD)."
+        echo -e "You may try to add your token ${COLOR}./bi.sh install [token]${NOCOLOR} as the way to get privately connected."
         return 0
     fi
+    echo "✔ Required files were received successfully."
     INC=0
-    if [ ! -f .secret-key-password ]; then
+    if [[ ! -f $DIR/_init/.secret-key-password ]]; then
         re='^[a-zA-Z]+$'
         while ! [[ ${DBPWD:0:1} =~ $re ]]
         do
             DBPWD=$(openssl rand -hex 12)
             let "INC+=1"
         done
-        echo $DBPWD > .secret-key-password
-        echo "Key password was created successfully ($INC)."
+        echo $DBPWD > $DIR/_init/.secret-key-password
     fi
-    echo "Required files were downloaded successfully :)"
     echo
-    echo "PLEASE MAKE THE NECESSARY CHANGES ON \"_bi/.env\" FILE"
-    echo "THEN RUN ./bi.sh up [--build] [--prod]"
+    echo "✔ Installation completed successfully."
+    echo
+    echo ">>> NEXT STEPS:"
+    echo -e ">>> 1. Open ${COLOR}_bi/.env${NOCOLOR} file with any text editor."
+    echo ">>> 2. Apply the necessary changes on the file and save it."
+    echo -e ">>> 3. Run ${COLOR}./bi.sh up${NOCOLOR} to start containers."
+    echo "Here you go!"
     return 1
 }
 
 function upBI() {
     if [ ! -d $DIR ]; then
-        echo "Oops! \"$DIR\" folder not found at the directory you are!"
-        echo "You need to install the platform by ./bi.sh install [token]"
+        echo
+        echo -e "Sorry! ${COLOR}$DIR${NOCOLOR} folder not found in the directory you are."
+        echo -e "You need to start installation with ${COLOR}./bi.sh install${NOCOLOR} command."
         return 0
     fi
-    if [ $1 ]; then
-        if [ $1 != "--build" ]; then
-            echo "Invalid parameter: $1"
-            return 0
-        else
-            BUILD=$(buildBI)
-        fi
+    UP=$(SUFFIX=$SUFFIX docker-compose -f $DIR/$DCYML up --detach --remove-orphans --no-build)
+    if [ ! $UP ]; then
+        echo
+        echo "✔ Platform was started successfully."
+        echo
+        echo "Please be patient up to 20 seconds for the system to be functional."
     fi
-    cd $DIR
-    DEV_SUFFIX=$GETSUFFIX docker-compose up --detach --remove-orphans --no-build
-    echo
-    echo "The platform has been started"
-    echo "PLEASE BE PAITENT UP TO 20 SECONDS FOR THE PLATFORM TO BE FUNCTIONAL"
     return 1
 }
 
-function downBI() {
-    cd $DIR
-    if [ $1 ]; then
-        echo "No parameter required: $1"
-        return 0
-    fi
-    DEV_SUFFIX=$GETSUFFIX docker-compose down
-    return 1
-}
-
-function pullBI() {
+function updateBI() {
     if [ ! -d $DIR ]; then
-        echo "Oops! \"$DIR\" folder not found at the directory you are!"
+        echo
+        echo -e "Sorry! ${COLOR}$DIR${NOCOLOR} folder not found in the directory you are."
         return 0
     fi
-    cd $DIR
-    DEV_SUFFIX=$GETSUFFIX docker-compose pull
-    echo "The latest software updates have been received successfully"
-    echo "RUN \"./bi.sh up [--build] [--prod]\" FOR CHANGES TO BE APPLIED"
+    PULL=$(SUFFIX=$SUFFIX docker-compose -f $DIR/$DCYML pull)
+    if [ ! $PULL ]; then
+        echo
+        echo "✔ The latest version received successfully."
+        echo -e "Please restart the platform with ${COLOR}./bi.sh up${NOCOLOR} command for the new version to be activated."
+    fi
     return 1
 }
 
 function pruneBI() {
-    if [ $1 ]; then
-        echo "No parameter required: $1"
-        return 0
-    fi
-    docker system prune
-    echo "Unused resources have been removed"
+    PRUNE=$(docker system prune --force)
+    echo $PRUNE
+    echo "✔ Dangling resources successfully removed."
     return 1
 }
 
-function buildBI() {
-    if [ ! $1 ]; then
-        echo "Please specify an image or add --all to build all images"
-        return 0
-    fi
-    for row in $(echo "${BUILDS}" | jq -r '.[] | @base64'); do
-        _jq() {
-            echo ${row} | base64 --decode | jq -r ${1}
-        }
-    IMAGE=$(echo $(_jq '.image'))
-    FOLDER=$(echo $(_jq '.folder'))
-    TAG=$(echo $(_jq '.tag'))
-    DOCKERFILE=$(echo $(_jq '.dockerfile'))
-    if [[ $1 == "--all" || $1 == $IMAGE ]]; then
-        docker build --tag $NS/$IMAGE$GETSUFFIX:$TAG --file $FOLDER/$DOCKERFILE $FOLDER/
-    fi
-    done
-    return 1
-}
-
-function killBI() {
-    if [ $1 ]; then
-        echo "No parameter required: $1"
-        return 0
-    fi
+function downBI() {
     if [[ ! -z $(docker container ls -qa) ]]; then
+        echo "Stopping and removing containers..."
         docker container stop $(docker container ls -qa)
         docker container rm $(docker container ls -qa)
-        echo "All containers have been stopped and removed successfully"
+        echo
+        echo "✔ All containers stopped and removed successfully."
     else
-        echo "No containers found!"
+        echo "No active container found!"
     fi
     return 1
 }
 
 # Setup
+COLOR='\033[0;31m'
+NOCOLOR='\033[0m'
 NS="ghcr.io/technoplatz"
 DIR="_bi"
 DCYML="docker-compose.yml"
 DOTENV=".env"
-BUILDS='[
-    {"folder":"_init","image":"bi-init","tag":"latest","dockerfile":"Dockerfile"},
-    {"folder":"_replicaset","image":"bi-replicaset","tag":"latest","dockerfile":"Dockerfile"},
-    {"folder":"api","image":"bi-api","tag":"latest","dockerfile":"Dockerfile"},
-    {"folder":"pwa","image":"bi-pwa","tag":"latest","dockerfile":"Dockerfile"}
-    ]'
-
-GETSUFFIX=$(getSuffixBI "$1" "$2" "$3")
-if [ "$GETSUFFIX" == "-1" ]; then
-    echo "Invalid command option: $1 $2 $3"
-    echo
-    exit 0
+SUFFIX=""
+if [ $2 ]; then
+    SUFFIX=$2
 fi
-echo "SUFFIX $GETSUFFIX"
 
 case $1 in
     "install")
@@ -244,27 +179,21 @@ case $1 in
     "up")
         upBI "$2"
         ;;
-    "build")
-        buildBI "$2"
-        ;;
-    "kill")
-        killBI "$2"
-        ;;
     "prune")
         pruneBI "$2"
         ;;
-    "pull")
-        pullBI "$2"
+    "update")
+        updateBI "$2"
         ;;
     "down")
         downBI "$2"
         ;;
     "help")
-        listAllCommands
+        helpBI
         ;;
     *)
-        echo "Command not found!"
-        listAllCommands
+        echo "Sorry! $1 is not an available command."
+        helpBI
 esac
 
 echo
